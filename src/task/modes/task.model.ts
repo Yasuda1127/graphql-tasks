ebff89f317bf9b1d075fe07c55e1e61d736c4c9e
Yuto Yasuda <yuto.yasuda@rakus-partners.co.jp>
2023-10-16 20:14:06 +0900

    modelsファイル作成途中 in models1

---
+++ src/task/modes/task.model.ts
@@ -0,0 +1,19 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class Task {
+  @Field(() => Int) // typescriptのフィールドをgraphqlのschemaのフィールドに変換する
+  id: number;
+
+  @Field()
+  name: string;
+
+  @Field()
+  dueDate: string;
+
+  @Field()
+  status: 'NOT_STARTED' | 'IN_PROGRESS' | 'COMPLETED';
+
+  @Field({ nullable: true })
+  description: string;
+}
